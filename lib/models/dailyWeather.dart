3a36ed38b83c9550f054f5dc19f78af39341b1f5
Ariz Armeidi <arizarmeidi@gmail.com>
2024-01-04 19:29:17 +0700

    Update dailyWeather.dart

--- lib/models/dailyWeather.dart
+++ lib/models/dailyWeather.dart
@@ -5,6 +5,10 @@ class DailyWeather with ChangeNotifier {
   final double temp;
   final double tempMin;
   final double tempMax;
+  final double tempMorning;
+  final double tempDay;
+  final double tempEvening;
+  final double tempNight;
   final String weatherCategory;
   final String condition;
   final DateTime date;
@@ -15,6 +19,10 @@ class DailyWeather with ChangeNotifier {
     required this.temp,
     required this.tempMin,
     required this.tempMax,
+    required this.tempMorning,
+    required this.tempDay,
+    required this.tempEvening,
+    required this.tempNight,
     required this.weatherCategory,
     required this.condition,
     required this.date,
@@ -27,6 +35,10 @@ class DailyWeather with ChangeNotifier {
       temp: (json['temp']['day']).toDouble(),
       tempMin: (json['temp']['min']).toDouble(),
       tempMax: (json['temp']['max']).toDouble(),
+      tempMorning: (json['feels_like']['morn']).toDouble(),
+      tempDay: (json['feels_like']['day']).toDouble(),
+      tempEvening: (json['feels_like']['eve']).toDouble(),
+      tempNight: (json['feels_like']['night']).toDouble(),
       weatherCategory: json['weather'][0]['main'],
       condition: json['weather'][0]['description'],
       date: DateTime.fromMillisecondsSinceEpoch(json['dt'] * 1000, isUtc: true),
