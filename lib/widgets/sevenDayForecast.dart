0466fb4c3f1533ab26b63ef3cd178b2196f87a41
Ariz Armeidi <arizarmeidi@gmail.com>
2024-01-18 21:02:06 +0700

    Update sevenDayForecast.dart

--- lib/widgets/sevenDayForecast.dart
+++ lib/widgets/sevenDayForecast.dart
@@ -56,67 +56,69 @@ class SevenDayForecast extends StatelessWidget {
                 shrinkWrap: true,
                 itemBuilder: (context, index) {
                   final DailyWeather weather = weatherProv.dailyWeather[index];
-                  return InkWell(
-                    onTap: () {
-                      Navigator.of(context).pushNamed(
-                        SevenDayForecastDetail.routeName,
-                        arguments: index,
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(12.0),
-                      decoration: BoxDecoration(
-                          color: index.isEven ? backgroundWhite : Colors.white,
-                          borderRadius: BorderRadius.circular(12.0)),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SizedBox(
-                            width: MediaQuery.sizeOf(context).width / 4,
-                            child: FittedBox(
-                              alignment: Alignment.centerLeft,
-                              fit: BoxFit.scaleDown,
-                              child: Text(
-                                index == 0
-                                    ? 'Today'
-                                    : DateFormat('EEEE').format(weather.date),
-                                style: semiboldText,
-                                maxLines: 1,
-                              ),
-                            ),
-                          ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SizedBox(
-                                height: 36.0,
-                                width: 36.0,
-                                child: Image.asset(
-                                  getWeatherImage(weather.weatherCategory),
-                                  fit: BoxFit.cover,
+                  return Material(
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: index.isEven ? backgroundWhite : Colors.white,
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(12.0),
+                      onTap: () {
+                        Navigator.of(context).pushNamed(
+                          SevenDayForecastDetail.routeName,
+                          arguments: index,
+                        );
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(
+                              width: MediaQuery.sizeOf(context).width / 4,
+                              child: FittedBox(
+                                alignment: Alignment.centerLeft,
+                                fit: BoxFit.scaleDown,
+                                child: Text(
+                                  index == 0
+                                      ? 'Today'
+                                      : DateFormat('EEEE').format(weather.date),
+                                  style: semiboldText,
+                                  maxLines: 1,
                                 ),
                               ),
-                              const SizedBox(height: 4.0),
-                              Text(
-                                weather.weatherCategory,
-                                style: lightText,
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            width: MediaQuery.sizeOf(context).width / 5,
-                            child: FittedBox(
-                              alignment: Alignment.centerLeft,
-                              fit: BoxFit.scaleDown,
-                              child: Text(
-                                weatherProv.isCelsius
-                                    ? '${weather.tempMax.toStringAsFixed(0)}°/${weather.tempMin.toStringAsFixed(0)}°'
-                                    : '${weather.tempMax.toFahrenheit().toStringAsFixed(0)}°/${weather.tempMin.toFahrenheit().toStringAsFixed(0)}°',
-                                style: semiboldText,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                  height: 36.0,
+                                  width: 36.0,
+                                  child: Image.asset(
+                                    getWeatherImage(weather.weatherCategory),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                const SizedBox(height: 4.0),
+                                Text(
+                                  weather.weatherCategory,
+                                  style: lightText,
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              width: MediaQuery.sizeOf(context).width / 5,
+                              child: FittedBox(
+                                alignment: Alignment.centerLeft,
+                                fit: BoxFit.scaleDown,
+                                child: Text(
+                                  weatherProv.isCelsius
+                                      ? '${weather.tempMax.toStringAsFixed(0)}°/${weather.tempMin.toStringAsFixed(0)}°'
+                                      : '${weather.tempMax.toFahrenheit().toStringAsFixed(0)}°/${weather.tempMin.toFahrenheit().toStringAsFixed(0)}°',
+                                  style: semiboldText,
+                                ),
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   );
